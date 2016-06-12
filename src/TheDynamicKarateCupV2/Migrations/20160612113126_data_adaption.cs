33cc7e47a8e561a6073589915ae7f049cf634d52
Tom <taisho.lede@gmail.com>
2016-06-12 13:35:59 +0200

    updates

---
+++ src/TheDynamicKarateCupV2/Migrations/20160612113126_data_adaption.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace TheDynamicKarateCupV2.Migrations
+{
+    public partial class data_adaption : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_IdentityRoleClaim<string>_IdentityRole_RoleId", table: "AspNetRoleClaims");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserClaim<string>_ApplicationUser_UserId", table: "AspNetUserClaims");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserLogin<string>_ApplicationUser_UserId", table: "AspNetUserLogins");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserRole<string>_IdentityRole_RoleId", table: "AspNetUserRoles");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserRole<string>_ApplicationUser_UserId", table: "AspNetUserRoles");
+            migrationBuilder.DropForeignKey(name: "FK_Coach_Club_ClubID", table: "Coach");
+            migrationBuilder.DropForeignKey(name: "FK_Competitor_Club_ClubID", table: "Competitor");
+            migrationBuilder.DropForeignKey(name: "FK_CompetitorCategory_Category_CategoryID", table: "CompetitorCategory");
+            migrationBuilder.DropForeignKey(name: "FK_CompetitorCategory_Competitor_CompetitorID", table: "CompetitorCategory");
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityRoleClaim<string>_IdentityRole_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserClaim<string>_ApplicationUser_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserLogin<string>_ApplicationUser_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserRole<string>_IdentityRole_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserRole<string>_ApplicationUser_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coach_Club_ClubID",
+                table: "Coach",
+                column: "ClubID",
+                principalTable: "Club",
+                principalColumn: "ClubID",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_Competitor_Club_ClubID",
+                table: "Competitor",
+                column: "ClubID",
+                principalTable: "Club",
+                principalColumn: "ClubID",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompetitorCategory_Category_CategoryID",
+                table: "CompetitorCategory",
+                column: "CategoryID",
+                principalTable: "Category",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompetitorCategory_Competitor_CompetitorID",
+                table: "CompetitorCategory",
+                column: "CompetitorID",
+                principalTable: "Competitor",
+                principalColumn: "CompetitorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_IdentityRoleClaim<string>_IdentityRole_RoleId", table: "AspNetRoleClaims");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserClaim<string>_ApplicationUser_UserId", table: "AspNetUserClaims");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserLogin<string>_ApplicationUser_UserId", table: "AspNetUserLogins");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserRole<string>_IdentityRole_RoleId", table: "AspNetUserRoles");
+            migrationBuilder.DropForeignKey(name: "FK_IdentityUserRole<string>_ApplicationUser_UserId", table: "AspNetUserRoles");
+            migrationBuilder.DropForeignKey(name: "FK_Coach_Club_ClubID", table: "Coach");
+            migrationBuilder.DropForeignKey(name: "FK_Competitor_Club_ClubID", table: "Competitor");
+            migrationBuilder.DropForeignKey(name: "FK_CompetitorCategory_Category_CategoryID", table: "CompetitorCategory");
+            migrationBuilder.DropForeignKey(name: "FK_CompetitorCategory_Competitor_CompetitorID", table: "CompetitorCategory");
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityRoleClaim<string>_IdentityRole_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserClaim<string>_ApplicationUser_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserLogin<string>_ApplicationUser_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserRole<string>_IdentityRole_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentityUserRole<string>_ApplicationUser_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coach_Club_ClubID",
+                table: "Coach",
+                column: "ClubID",
+                principalTable: "Club",
+                principalColumn: "ClubID",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_Competitor_Club_ClubID",
+                table: "Competitor",
+                column: "ClubID",
+                principalTable: "Club",
+                principalColumn: "ClubID",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompetitorCategory_Category_CategoryID",
+                table: "CompetitorCategory",
+                column: "CategoryID",
+                principalTable: "Category",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompetitorCategory_Competitor_CompetitorID",
+                table: "CompetitorCategory",
+                column: "CompetitorID",
+                principalTable: "Competitor",
+                principalColumn: "CompetitorID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
